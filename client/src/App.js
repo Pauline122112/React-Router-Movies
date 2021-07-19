1325afc5c5df1663b0f490cc80a984113311eece
Pauline122112 <pauline.design007@gmail.com>
2021-07-19 17:12:07 -0500

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Link, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList'
 import Movie from './Movies/Movie'
+import MovieCard from './Movies/MovieCard'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
